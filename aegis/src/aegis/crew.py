0f91b684fe956bba42b3a4c0db10bb3bc5f00e94
preethamnagesh8 <preethamnagesh8@Preethams-MacBook-Pro.local>
2024-11-07 23:55:37 -0700

    Aegis MVP

--- aegis/src/aegis/crew.py
+++ aegis/src/aegis/crew.py
@@ -1,5 +1,6 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
+from aegis.tools.custom_tool import WordPressCreateBlog
 
 # Uncomment the following line to use an example of a custom tool
 # from aegis.tools.custom_tool import MyCustomTool
@@ -15,28 +16,82 @@ class AegisCrew():
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+			verbose=True
+		)
+	
+	@agent
+	def content_planner(self) -> Agent:
+		return Agent(
+			config=self.agents_config['content_planner'],
+			verbose=True
+		)
+	
+	@agent
+	def content_expander(self) -> Agent:
+		return Agent(
+			config=self.agents_config['content_expander'],
+			tools = [WordPressCreateBlog()],
 			verbose=True
 		)
 
 	@agent
-	def reporting_analyst(self) -> Agent:
+	def storyline_generator(self) -> Agent:
 		return Agent(
-			config=self.agents_config['reporting_analyst'],
+			config=self.agents_config['storyline_generator'],
 			verbose=True
 		)
+	
+	@agent
+	def blog_formatter(self) -> Agent:
+		return Agent(
+			config=self.agents_config['blog_formatter'],
+			verbose=True
+		)
+	
+	@agent
+	def blog_publisher(self) -> Agent:
+		return Agent(
+			config=self.agents_config['blog_publisher'],
+			tools = [WordPressCreateBlog()],
+			verbose=True
+		)
+	
+	# Tasks from here --------------------
 
 	@task
 	def research_task(self) -> Task:
 		return Task(
 			config=self.tasks_config['research_task'],
 		)
+	
+	@task
+	def content_planning_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['content_planning_task'],
+		)
+	
+	@task
+	def content_expansion_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['content_expansion_task'],
+		)
 
 	@task
-	def reporting_task(self) -> Task:
+	def storyline_development_task(self) -> Task:
 		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			config=self.tasks_config['storyline_development_task'],
+		)
+	
+	@task
+	def html_formatting_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['html_formatting_task'],
+		)
+	
+	@task
+	def blog_publishing_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['blog_publishing_task'],
 		)
 
 	@crew
--- aegis/src/aegis/tools/custom_tool.py
+++ aegis/src/aegis/tools/custom_tool.py
@@ -1,12 +1,41 @@
 from crewai_tools import BaseTool
+from pydantic import BaseModel, Field
+from typing import Any, Optional, Type
+import requests
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config_path = os.path.join(os.path.dirname(__file__), "..", "settings", "aegis_config.local")
+config.read(config_path)
+
+class WordPressCreateBlogSchema(BaseModel):
+    """Input for WordPressCreateBlog"""
+    title: str = Field(..., description = "Mandatory title field for blog")
+    content: str = Field(..., description = "Mandatory body with text data for blog")
 
 
-class MyCustomTool(BaseTool):
-    name: str = "Name of my tool"
+class WordPressCreateBlog(BaseTool):
+    name: str = "Post a blog in wordpress"
     description: str = (
-        "Clear description for what this tool is useful for, you agent will need this information to use it."
+        "A tool that can post a blog to wordpress. This is an API call that makes a POST request to post data to wordpress site."
     )
+    args_schema: Type[BaseModel] = WordPressCreateBlogSchema
 
-    def _run(self, argument: str) -> str:
-        # Implementation goes here
-        return "this is an example of a tool output, ignore it and move along."
+    def _run(self, title: str, content: str) -> str:
+        
+        data = {
+            "title": title,
+            "content": content
+        }
+
+        wordpress_api_token = config.get("wordpress", "wordpress_api_key")
+        blog_post_url = config.get("wordpress", "wordpress_blog_post_url")
+
+        headers = {
+            "Authorization": f"Bearer {wordpress_api_token}",
+            "Content-Type": "application/json"
+        }
+
+        response = requests.post(blog_post_url, json=data, headers=headers)
+        return response.text
